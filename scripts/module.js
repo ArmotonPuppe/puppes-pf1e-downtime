b58be41d33cf47020a67b5190782ede981b8db4b
Puppe <parkkinenjesper@gmail.com>
2026-04-01 03:50:07 +0300

    More testing and learning

--- scripts/module.js
+++ scripts/module.js
@@ -1,3 +1,14 @@
+Hooks.once("init", () => {
+  game.settings.register("puppes-pf1e-downtime", "showDowntime", {
+    name: "Show downtime tab",
+    hint: "Makes downtime tab on character sheets",
+    scope: "world",
+    config: true,
+    type: Boolean,
+    default: true,
+  });
+
+});
 Hooks.once("ready", function(){
   console.log("I am a loaded module");
 });
